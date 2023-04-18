625179c0b1a635f5c4570357c05795aacdd6d651
PawsThePaw <124329589+PawsThePaw@users.noreply.github.com>
2023-04-18 18:53:46 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -1,6 +1,5 @@
 --This Script is Copyrighted If you Managed To Deobfuscate Well then Keep it To yourself Or I will sue yo momma
 --This is The auto Update shit If u use the loadstring Down here You won't get Updates
 --Aspect X is Recontinued BTW!
---This script is Skidded But I gave Credits.
 --use the Loadstring Provided And not This.
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/Plutonium.txt"))()
