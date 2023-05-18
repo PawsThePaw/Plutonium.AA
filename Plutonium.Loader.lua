ced2af02019f1219d877d944ff5e0a50cd051af3
PawsThePaw <124329589+PawsThePaw@users.noreply.github.com>
2023-05-18 21:08:54 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -2,4 +2,18 @@
 --This is The auto Update shit If u use the loadstring Down here You won't get Updates
 --Aspect X is Recontinued BTW!
 --use the Loadstring Provided And not This.
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/Plutonium.lua"))()
+
+if Drawing then
+
+        print("Drawing.new Supported")
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/pluto/main/Plutonium1.txt"))()
+
+else 
+
+        print("Drawing.new Not Supported")
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/pluto/main/Plutonium2.txt"))()
+
+end
+
