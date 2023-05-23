85235e9ae12e50a73e806f9468d36575404caf7a
PawsThePaw <124329589+PawsThePaw@users.noreply.github.com>
2023-05-23 19:16:51 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -7,7 +7,7 @@
 
 
 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/PLUTONIUMVERSIONS/main/PlutoPc.txt"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/77_CBKSITO.lua.txt"))()
 
 
 
