6907ef62a3841f88e4783f4573c6b561c25b1db7
Paws <124329589+PawsThePaw@users.noreply.github.com>
2023-05-28 07:51:09 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -2,12 +2,12 @@
 --This is The auto Update shit If u use the loadstring Down here You won't get Updates
 --Aspect X is Recontinued BTW!
 --use the Loadstring Provided And not This.
+--Usually Gets Updated Every atleast once in 2 weeks
 
 
 
 
-
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/77_CBKSITO.lua.txt"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/77_UCOULFZTPXWM06.lua.txt"))()
 
 
 
