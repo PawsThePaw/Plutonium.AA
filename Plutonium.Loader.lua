acbbaa3349266c370e685b7c4c56fcfee675d9ce
Paws <124329589+PawsThePaw@users.noreply.github.com>
2023-11-02 09:40:11 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -7,7 +7,7 @@
 
 
 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/V3.0.6.txt"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/V3.0.7.txt"))()
 
 
 
