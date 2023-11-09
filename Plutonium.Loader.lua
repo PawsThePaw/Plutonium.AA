c2a71ff13d40a65aaa51eb6111deae5a0e39e97c
Paws <124329589+PawsThePaw@users.noreply.github.com>
2023-11-09 20:36:30 +0800

    Update Plutonium.Loader.lua
    Stolen Crisis Kick.

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -4,7 +4,7 @@
 --// • Version: V3.1.0
 --// • Discord Server: https://discord.gg/vKfJNqC2US
 --// • Developers: Paws
-
+game.Players.LocalPlayer:Kick("Hello, This Is The Creator Of The Script, I Am Terribly Sorry For Plutonium Being Down, It Is Due For It Being Stolen And Used Without Permission, Plutonium V3 Is Not Discontinued, It Is Just Facing Down Time, Come Back Another Time! Issue Will Be Resolved Soon - Plutonium V3 Owner, PawsThePaw")
 
 
 
