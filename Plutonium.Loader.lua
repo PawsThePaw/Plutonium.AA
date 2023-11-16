a30d3ccbbf9774ea1cd38a5e47f7b2a07b0de7b3
Paws <124329589+PawsThePaw@users.noreply.github.com>
2023-11-16 20:15:33 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -1,22 +1,11 @@
 --//Hello This Is Paws, Please Do Not Use This Loadstring Because This Is The Reason How Your Getting Updates.
 
 --// • Obfuscated: Yes
---// • Version: V3.1.0
+--// • Version: V3.1.3
 --// • Discord Server: https://discord.gg/vKfJNqC2US
 --// • Developers: Paws
-game.StarterGui:SetCore("SendNotification", {
-Title = "Important Notification";
-Text = " PawsThePaw, OUR OFFICIAL DISCORD SERVER IS AT: https://discord.gg/NttVPHV7WA IF YOU GOT THIS SCRIPT FROM A SERVER NAMED YU HUB THEY JUST STOLE MY STUFF";
-Icon = ""; 
-Duration = 45;
-})
-game.StarterGui:SetCore("SendNotification", {
-Title = "Important Notification";
-Text = "https://discord.gg/NttVPHV7WA PLEASE LEAVE YU HUB DISCORD SERVER IMMEDIATELY THEY ARE JUST STEALING STUFF FROM ME! - PawsThePaw";
-Icon = ""; 
-Duration = 45;
-})
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/3.1.2.txt", true))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaws/Test/main/V3.1.3.txt", true))()
 
 
 
