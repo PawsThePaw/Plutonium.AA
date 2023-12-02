054e53a2f24bee0e1829af65b7f2ace4319a93b1
Paws <124329589+PawsThePaw@users.noreply.github.com>
2023-12-02 13:11:27 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -1,11 +1,11 @@
 --//Hello This Is Paws, Please Do Not Use This Loadstring Because This Is The Reason How Your Getting Updates.
 
 --// • Obfuscated: Yes
---// • Version: 3.1.9
+--// • Version: 3.2.0
 --// • Discord Server: https://discord.gg/vKfJNqC2US
 --// • Developers: PawsThePaw
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/V3.1.9.txt", true))()
+--// • Owner: PawsThePaw
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/V3.2.0.txt", true))()
 
 
 
