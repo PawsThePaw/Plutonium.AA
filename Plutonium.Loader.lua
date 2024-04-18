3ffbdaae9780f498f3ac7c7436fb5dc65055604e
Paws <124329589+PawsThePaw@users.noreply.github.com>
2024-04-18 04:33:18 -0400

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -1,15 +1,15 @@
 --//Hello This Is Paws, Please Do Not Use This Loadstring Because This Is The Reason How Your Getting Updates.
 
 --// • Obfuscated: Yes
---// • Version: 3.5.8
---// • Discord Server: https://discord.gg/vKfJNqC2US
+--// • Version: 3.5.9
+--// • Discord Server: https://discord.gg/plutoniumhub
 --// • Developers: PawsThePaw, Dexpt, Silver
 --// • Owner: PawsThePaw
 --// • Average Updates Per Week: 1 - 3
 --// • Fps Drops Estimate: 1 - 5 FPS
 --// • Great When Used On [Ping]: 0 - 300ms
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Test/main/Secret.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/V3.5.9.txt", true))()
 
 
 
