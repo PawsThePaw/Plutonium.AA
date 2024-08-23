4bedea20c36c90da3ae126626279b0337d6bbc97
Paws <124329589+PawsThePaw@users.noreply.github.com>
2024-08-23 15:43:51 +0800

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -1,12 +1,28 @@
---//Hello This Is Paws, Please Do Not Use This Loadstring Because This Is The Reason How Your Getting Updates.
+--[[
 
---// • Obfuscated: Yes
---// • Version: 3.6.3
---// • Discord Server: https://discord.com/invite/rGVUA8Hp
---// • Developers: PawsThePaw, Dexpt
+
+██████╗ ██╗     ██╗   ██╗████████╗ ██████╗ ███╗   ██╗██╗██╗   ██╗███╗   ███╗   ██╗     ██╗   ██╗ █████╗ 
+██╔══██╗██║     ██║   ██║╚══██╔══╝██╔═══██╗████╗  ██║██║██║   ██║████╗ ████║   ██║     ██║   ██║██╔══██╗
+██████╔╝██║     ██║   ██║   ██║   ██║   ██║██╔██╗ ██║██║██║   ██║██╔████╔██║   ██║     ██║   ██║███████║
+██╔═══╝ ██║     ██║   ██║   ██║   ██║   ██║██║╚██╗██║██║██║   ██║██║╚██╔╝██║   ██║     ██║   ██║██╔══██║
+██║     ███████╗╚██████╔╝   ██║   ╚██████╔╝██║ ╚████║██║╚██████╔╝██║ ╚═╝ ██║██╗███████╗╚██████╔╝██║  ██║
+╚═╝     ╚══════╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝
+ 
+                                                                                                       
+--]]
+
+
+--// • MAIN DETAILS • \\ --
+
 --// • Owner: PawsThePaw
+--// • Obfuscated: Yes
+--// • Version: 3.6.4
+--// • Libraries & Modules: 6
+--// • Developement Speed: Very Slow
+--// • Discord Server: https://discord.com/invite/rGVUA8Hp
+--// • Developers: PawsThePaw
 --// • Average Updates Per Week: 1 - 3
---// • Fps Drops Estimate: 1 - 5 FPS
---// • Great When Used On [Ping]: 0 - 300ms
+--// • Average Load Time: 0 - 10 Seconds
+--// • Average System Downtimes: N/A
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/main/Versions/V3.6.4.txt", true))()
