c89fd27db38c899a00b0498308c94c4ec43ee1c4
Paws <124329589+PawsThePaw@users.noreply.github.com>
2025-04-02 00:02:19 -0400

    Update Plutonium.Loader.lua

--- Plutonium.Loader.lua
+++ Plutonium.Loader.lua
@@ -29,7 +29,7 @@
 
 --// • Owner: PawsThePaw
 --// • Obfuscated: Yes
---// • Version: APRIL FOOLS
+--// • Version: V3.7.8
 --// • Libraries & Modules: 6
 --// • Developement Speed: Very Slow
 --// • Discord Server: https://discord.com/invite/rGVUA8Hp
@@ -38,13 +38,4 @@
 --// • Average Load Time: 0 - 10 Seconds
 --// • Average System Downtimes: N/A
 
-game.StarterGui:SetCore("SendNotification", {
-Title = "Plutonium.lua";
-Text = "Copied a Normal Version of the script in your clipboard! If you do not wish to participate in this April Fools Event Please Use that one instead.";
-Duration = 10
-})
-
-setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/refs/heads/main/Versions%202/V3.7.7.txt", true))()')
-task.wait(10)
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/refs/heads/main/April%20Fools%20Versions/April%20Fools%202025.txt", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/refs/heads/main/Versions%202/V3.7.8.txt", true))()
